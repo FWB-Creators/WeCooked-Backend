daf2561a51fcf9a774430f285cc65f563928a648
Billy19191 <bill.ratchanon@gmail.com>
2024-10-06 22:40:44 +0700

    feat: integrate Prisma with database and add User model with migrations

--- test-service/src/app.service.ts
+++ test-service/src/app.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class AppService {
+export class AppService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Connected to the database');
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
 
-  createUser(createUserRequest: any): string {
+  async createUser(createUserRequest: any): Promise<Record<string, any>> {
+    // const { name, email } = createUserRequest; // Destructure the object
     console.log('create user!! - test service', createUserRequest);
-    return 'User created!';
+    console.log(createUserRequest.createUserRequest);
+    console.log(
+      'create user!! - test service',
+      createUserRequest.createUserRequest.name,
+      createUserRequest.createUserRequest.email,
+    );
+
+    // Wait until parameters are provided
+
+    return this.user.create({
+      data: {
+        name: createUserRequest.createUserRequest.name,
+        email: createUserRequest.createUserRequest.email,
+      },
+    });
   }
 
   getUsers(): Record<string, any>[] {
