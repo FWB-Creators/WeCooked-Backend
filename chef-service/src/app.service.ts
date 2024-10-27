370d47d7fe4b84421efc2a5371a1ee7bc7a0a19d
Billy19191 <bill.ratchanon@gmail.com>
2024-10-27 23:48:56 +0700

    feat: prisma generate across service

--- chef-service/src/app.service.ts
+++ chef-service/src/app.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../gateway/node_modules/.prisma/client';
 
 @Injectable()
-export class AppService {
+export class AppService extends PrismaClient implements OnModuleInit {
+  onModuleInit() {
+    this.$connect();
+    console.log('Connected to the database');
+  }
   getChef(): string {
     return 'Chef is here';
   }
--- gateway/prisma/migrations/20241027163537_init/migration.sql
+++ gateway/prisma/migrations/20241027163537_init/migration.sql
@@ -1,14 +1,14 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
