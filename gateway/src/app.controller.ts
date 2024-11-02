192c85b4a8b8e4e93a25b6a480dcc62d7a29c4ad
Billy19191 <bill.ratchanon@gmail.com>
2024-11-02 23:07:03 +0700

    feat: new database schema model

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -1,20 +1,23 @@
 import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Observable } from 'rxjs';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   logger = new Logger('Gateway Service');
+
+  @ApiTags('Chef')
   @Get('chef/:action?')
   handleChefAction(@Param('action') action: string): Observable<any> {
     this.logger.log('Action:', action);
     if (!action) {
       return this.appService.getChef();
     }
-    // Handle other actions if needed
   }
 
+  @ApiTags('Chef')
   @Post('chef/:action')
   handleChefPostAction(
     @Param('action') action: string,
