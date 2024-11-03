76e11505e749051b0cc097ea81183d532a605a06
AungKhantThaw47 <aungkthaw@gmail.com>
2024-11-04 03:19:39 +0700

    prisma still err

--- gateway-service/src/app.controller.ts
+++ gateway-service/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post , Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { Prisma, User as UserModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   async getHelloToUserService(): Promise<string> {
     return await this.appService.getHelloToUserService();
+  }
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
   }
 }
--- gateway-service/src/app.module.ts
+++ gateway-service/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule {}
