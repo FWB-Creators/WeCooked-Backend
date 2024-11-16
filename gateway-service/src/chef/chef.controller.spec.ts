ae9c4635d9ce6a2005d4afb8da01208a49817160
Billy19191 <bill.ratchanon@gmail.com>
2024-11-16 23:51:57 +0700

    feat: refactor app module to chef entity in gateway

--- gateway-service/src/chef/chef.controller.spec.ts
+++ gateway-service/src/chef/chef.controller.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+<<<<<<<< HEAD:gateway-service/src/chef/chef.controller.spec.ts
 import { ChefController } from './chef.controller';
 import { ChefService } from './chef.service';
+========
+import { AppController } from './chef.controller';
+import { AppService } from './chef.service';
+>>>>>>>> f639b2ea (feat: refactor app module to chef entity in gateway):gateway/src/entity/chef.controller.spec.ts
 
 describe('AppController', () => {
   let appController: ChefController;
