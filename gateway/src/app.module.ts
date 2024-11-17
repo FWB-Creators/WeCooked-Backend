a0513b771c225ae4ffa431abf213e2c7ab8e65ff
Billy19191 <bill.ratchanon@gmail.com>
2024-11-17 18:10:12 +0700

    feat: implement http exception for sign up chef

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,6 +1,12 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ChefController } from './entity/chef.controller';
-import { ChefService } from './entity/chef.service';
+import {
+  HttpException,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { ChefController } from './chef/chef.controller';
+import { ChefService } from './chef/chef.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './middleware/logger.middleware';
@@ -21,7 +27,13 @@ import { UserModule } from './user/user.module';
     UserModule,
   ],
   controllers: [ChefController],
-  providers: [ChefService],
+  providers: [
+    ChefService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpException,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
