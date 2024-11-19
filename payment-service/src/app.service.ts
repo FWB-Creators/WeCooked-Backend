691bb2d2fa8262cbf02cb14f63cdd8c61cf1f8bf
AungKhantThaw47 <aungkthaw@gmail.com>
2024-11-19 19:44:25 +0700

    single schema

--- payment-service/src/app.service.ts
+++ payment-service/src/app.service.ts
@@ -1,17 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { StripeService } from './stripe/stripe.service';
+import { BasicResponse } from '@lib/src/user/event-msg.dto';
 
 @Injectable()
-export class AppService {
-  constructor(private readonly stripeService: StripeService) {}
+export class AppService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly stripeService: StripeService) {
+    super();
+  }
+  logger = new Logger('Payment Service');
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Connected to the database');
+    const test: BasicResponse = {
+      message: 'Hello World',
+      status: 200,
+    };
+    console.log(test);
+  }
+
   async getHeartbeat(): Promise<string> {
     const session = await this.stripeService.stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
-            currency: 'usd',
+            currency: 'thb',
             product_data: {
-              name: 'T-shirt',
+              name: 'Course Name',
+              metadata: {},
             },
             unit_amount: 2000,
           },
@@ -19,7 +35,36 @@ export class AppService {
         },
       ],
       mode: 'payment',
-      success_url: 'https://example.com/success',
+      success_url: 'http://example.com/success',
+    });
+    console.log(session);
+
+    return session.url;
+  }
+
+  async createCheckoutSessionForCourse(
+    courseId: string,
+    userId: number,
+  ): Promise<string> {
+    const session = await this.stripeService.stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'thb',
+            product_data: {
+              name: 'Course Name',
+              metadata: {
+                courseId: courseId,
+                userId: userId,
+              },
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://example.com/success',
     });
     console.log(session);
 
